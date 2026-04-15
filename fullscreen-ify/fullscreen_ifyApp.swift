3e8c7e1a0d7609f038dabf5addcc1773d2e01631
Shivansh Agarwal <shivansh.aeyron@gmail.com>
2026-04-15 21:33:45 +0530

    Initial Commit

---
+++ fullscreen-ify/fullscreen_ifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  fullscreen_ifyApp.swift
+//  fullscreen-ify
+//
+//  Created by Shivansh Agarwal on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct fullscreen_ifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
